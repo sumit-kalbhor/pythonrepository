c2d4a3ba622ed316836faac15964fbf0a38d51c8
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-24 17:05:46 +0530

    python strings and string methods

---
+++ string.py
@@ -0,0 +1,19 @@
+# convert string in upper case
+name = "my name is sumit kalbhor."
+print(name.upper())
+
+# convert string to lower case
+age = "MY AGE IS 24 YEARS."
+print(age.lower())
+
+# string methods
+print("SUMIT".isupper())
+print("sumit".islower())
+print("123".isdecimal())
+print("    ".isspace())
+print("sumit".startswith("s"))
+print("sumit".startswith("u"))
+print("sumit".endswith("t"))
+print("sumit".endswith("S"))
+print(", ".join(["1", "2", "3", "4"]))
+print("one,two,three,four".split(","))
