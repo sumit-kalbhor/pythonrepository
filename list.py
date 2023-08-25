d85356df375109b0e677b8b0122e03d054638e40
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-25 11:24:49 +0530

    python list

---
+++ list.py
@@ -0,0 +1,16 @@
+# create a list
+int_list = [1, 2, 3, 4]
+print(int_list)
+
+mixed_list = [10.00, "Sumit", 1, True]
+print(mixed_list)
+
+# list() function creates a list
+name = list("SUMIT")
+print(name)
+
+# in or not in operators
+print('S' in name)
+print('K' in name)
+print('K' not in name)
+
