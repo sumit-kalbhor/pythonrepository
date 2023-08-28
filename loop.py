191bd32eecfed39ae4a871fcf579caceed68aa0e
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-28 13:00:18 +0530

    python loops

---
+++ loop.py
@@ -0,0 +1,43 @@
+
+# if else
+
+i = 10
+if i > 11 :
+    print("10 is greater than 11")
+else :
+    print("10 is less than 11")
+
+# if elif else
+
+i = 5
+
+if i == 1:
+    print("1 is equal to 5")
+elif i == 3:
+    print("3 is equal to 5")
+elif i == 5:
+    print("5 is equal to 5")
+else:
+    print("Not matching")
+
+
+# for loop
+
+str = "Hello World"
+
+for i in str:
+    print(i)
+
+
+sum = 0
+for i in range(1,11):
+    print(i)
+
+    sum += i
+    print(sum)
+
+
+
+
+
+
