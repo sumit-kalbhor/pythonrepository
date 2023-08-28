477ef8d07ce3ed7cc4ce9326acd94d209bec3cf4
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-28 16:54:08 +0530

    python class

---
+++ Employee.py
@@ -0,0 +1,18 @@
+# create a class
+class Employee:
+    name = "Sumit Kalbhor"
+    designation = "Associate I"
+
+    # method
+    def grade(self):
+        if self.designation == "Associate I":
+            print("Grade belongs to A")
+        else:
+            print("Not belongs to A grade")
+
+
+# object creation
+employeeOne = Employee()
+
+# calling a method from class
+employeeOne.grade()
