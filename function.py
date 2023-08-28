c8bf6e39c19863f06151875440c186ec2adf9875
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-28 14:51:12 +0530

    python functions

---
+++ function.py
@@ -0,0 +1,24 @@
+# define a function
+def name():
+    name = input("Enter a name : ")
+    print(name)
+
+name()
+
+
+# swap the two numbers
+
+def swap(x, y):
+    temp = x
+    x = y
+    y = temp
+    print(x)
+    print(y)
+
+
+x = 2
+y = 7
+swap(x, y)
+
+
+
