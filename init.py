080e04223fc5ec650a3ced2ac31c223901ccc280
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-08-29 12:18:40 +0530

    __init__() method is added

---
+++ init.py
@@ -0,0 +1,14 @@
+class Employee:
+
+    def __init__(self):
+        self.name = "Sumit"
+
+    def displayDetails(self):
+        print(self.name)
+
+
+employee = Employee()
+employee.displayDetails()
+
+
+
