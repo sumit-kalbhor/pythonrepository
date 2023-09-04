ac21ddc71c8d1a13f4b2271b089bc838b0a820a0
sumit-kalbhor <sumitkalbhor1512@gmail.com>
2023-09-04 11:28:09 +0530

    generator function in python

---
+++ generator.py
@@ -0,0 +1,18 @@
+# create generator function
+def number():
+    yield 1
+    yield 2
+    yield "Hello World"
+
+
+# for loop method
+for i in number():
+    print(i)
+
+# x is a generator object
+x = number()
+
+# next method for iterating generator object
+print(next(x))
+print(next(x))
+print(next(x))
